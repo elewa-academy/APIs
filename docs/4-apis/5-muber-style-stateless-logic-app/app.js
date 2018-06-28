c67ff677e0ea00e5021c7410c342432f5b70bc2b
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2018-06-28 19:11:55 +0200

    publishing: $1

--- docs/4-apis/5-muber-style-stateless-logic-app/app.js
+++ docs/4-apis/5-muber-style-stateless-logic-app/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes/routes');
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
+
+// cross origin resource sharing
+// to accept requests that weren't issued by an app served on this port
+//  a must for APIs
+app.use(cors());
 
 routes(app);
 
